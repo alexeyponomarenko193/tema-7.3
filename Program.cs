91494d2d067bfa1dd5610ff6153dac43dc95ee7e
Wanfogai <wanfogai770@gmail.com>
2023-03-06 09:43:21 +0200

    1

---
+++ Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace ConsoleApp33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Random random = new Random();
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    int x = random.Next(-40, 40);
+            //    Console.WriteLine(x);
+            //}
+            //Console.Read();
+            //int count = 0;
+            //Random random = new Random();
+            //for (int i = 1; i < 15; i++)
+            //{
+            //    double x = -20 + 50 * random.NextDouble();
+            //    Console.WriteLine(x);
+            //    if (x > 0)
+            //    {
+            //        count++;
+            //    }
+
+            //}
+
+            //Console.WriteLine(); 
+            //Console.ReadKey();
+
+            //{
+            //    Random random = new Random();
+
+            //    Console.WriteLine("Случайные числа:");
+            //    for (int i = 0; i < 12; i++)
+            //    {
+            //        double number = random.NextDouble() * (5 - (-5)) + (-5);
+            //        Console.Write(number + " ");
+
+            //        if (number < 0)
+            //        {
+            //            Console.WriteLine("- отрицательное число");
+            //        }
+            //        else if (number > 0)
+            //        {
+            //            Console.WriteLine("- положительное число");
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("- ноль");
+            //        }
+            //    }
+
+            //    Console.ReadKey();
+
+            //Random random = new Random();
+
+            //Console.WriteLine("Случайные числа:");
+            //for (int i = 0; i < 8; i++)
+            //{
+            //    int number = random.Next(-30, 31);
+            //    Console.Write(number + " ");
+
+            //    if (number < 0)
+            //    {
+            //        Console.Write("- отрицательное число, ");
+            //    }
+            //    else if (number > 0)
+            //    {
+            //        Console.Write("- положительное число, ");
+            //    }
+            //    else
+            //    {
+            //        Console.Write("- ноль, ");
+            //    }
+
+            //    if (number % 2 == 0)
+            //    {
+            //        Console.WriteLine("четное число");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("нечетное число");
+            //    }
+            //}
+
+            //Console.ReadKey();
+            //Random random = new Random();
+
+            //Console.WriteLine("Случайные положительные числа:");
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    int number = random.Next(-20, 21);
+
+            //    if (number > 0)
+            //    {
+            //        Console.Write(number + " ");
+
+            //        if (number % 2 == 0)
+            //        {
+            //            Console.WriteLine("- четное число");
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("- нечетное число");
+            //        }
+            //    }
+            //}
+
+            //Console.ReadKey();
+        }
+    }
+}
